996f0d6e3f62eb1aa3762de6b75b728e0df40823
aspirin100 <aspi.jb.ru@gmail.com>
2025-11-27 15:37:25 +0300

    port insertion in args + default port

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <cstdint>
+#include <cstdlib>
 
 #include "server.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    uint16_t port;
-    std::cout << "enter server port: " << std::endl;
-    std::cin >> port;
+    uint16_t port = 0;
 
+    if(argc > 1)
+        port = std::atoi(argv[1]);
+    
+    if(!port)
+        port = 8888;
+    
     Server server(port);
     server.Start();
 
